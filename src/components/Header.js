b3a5b558e34cded50ee43d34f9553c03d6251a2f
Sonal Sengar <sengarsonal13@gmail.com>
2024-10-11 18:56:09 +0530

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <>
+            <header className='header-top-strip py-3'>
+                <div className='container-xxl'>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <p className='text-white mb-0'>Free Shipping Over $100 & Free Returns</p>
+                        </div>
+                        <div className='col-6'>
+                            <p className='text-end text-white mb-0'>Hotline: <a href='tel: +919876543210' className='text-white'>+91 9876543210</a></p>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
     )
 }
 
-export default Header
+export default Header;
