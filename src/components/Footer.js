3e76bf02a65deec31a83c2ab53d40af3815b695f
Sonal Sengar <sengarsonal13@gmail.com>
2024-10-14 19:21:24 +0530

    completed footer part

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,105 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom';
+import { BsLinkedin, BsYoutube, BsInstagram, BsGithub, BsFacebook } from 'react-icons/bs'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+            <footer className='py-4'>
+                <div className="container-xxl">
+                    <div className="row align-items-center">
+                        <div className="col-5">
+                            <div className="footer-top-data d-flex align-items-center gap-30">
+                                <img src="images/newsletter.png" alt="newsletter" />
+                                <h3 className='mb-0 text-white'>Sign Up for Newsletter</h3>
+                            </div>
+                        </div>
+                        <div className="col-7">
+                            <div class="input-group">
+                                <input type="text" className="form-control py-1" placeholder="Your Email" aria-label="Your Email" aria-describedby="basic-addon2" />
+                                <span class="input-group-text" id="basic-addon2">Subscribe</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <footer className='py-4'>
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-4">
+                            <h4 className='mb-4 text-white'>Contact Us</h4>
+                            <div>
+                                <address className='text-white fs-6'>
+                                    Flat No. 201, Landmark Residency, <br />
+                                    Lane No.2 Veerbhadra Nagar, <br />
+                                    Baner, Pune 411045
+                                </address>
+                                <a href='tel:+919876543210' className='mt-3 d-block mb-1 text-white'>+91 98765 43210</a>
+                                <a href='mailto:sengarsonal13@gmail.com' className='mt-2 d-block mb-0 text-white'>sengarsonal13@gmail.com</a>
+                                <div className="social-icons d-flex align-items-center gap-15 mt-4">
+                                    <a href='' className='icon-circle'>
+                                        <BsFacebook className='text-white fs-4' />
+                                    </a>
+                                    <a href='' className='icon-circle'>
+                                        <BsGithub className='text-white fs-4' />
+                                    </a>
+                                    <a href='' className='icon-circle'>
+                                        <BsInstagram className='text-white fs-4' />
+                                    </a>
+                                    <a href='' className='icon-circle'>
+                                        <BsYoutube className='text-white fs-4' />
+                                    </a>
+                                    <a href='' className='icon-circle'>
+                                        <BsLinkedin className='text-white fs-4' />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <h4 className='mb-4 text-white'>Information</h4>
+                            <div className='footer-links d-flex flex-column'>
+                                <Link className='text-white py-2 mb-1'>Privacy Policy</Link>
+                                <Link className='text-white py-2 mb-1'>Shipping Policy</Link>
+                                <Link className='text-white py-2 mb-1'>Refund Policy</Link>
+                                <Link className='text-white py-2 mb-1'>Terms & Conditions</Link>
+                                <Link className='text-white py-2 mb-1'>Blogs</Link>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <h4 className='mb-4 text-white'>Account</h4>
+                            <div className='footer-links d-flex flex-column'>
+                                <Link className='text-white py-2 mb-1'>Search</Link>
+                                <Link className='text-white py-2 mb-1'>About Us</Link>
+                                <Link className='text-white py-2 mb-1'>Faq</Link>
+                                <Link className='text-white py-2 mb-1'>Contact</Link>
+                                <Link className='text-white py-2 mb-1'>Size Chart</Link>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <h4 className='mb-4 text-white'>Quick Links</h4>
+                            <div className='footer-links d-flex flex-column'>
+                                <Link className='text-white py-2 mb-1'>Accessories</Link>
+                                <Link className='text-white py-2 mb-1'>Laptops</Link>
+                                <Link className='text-white py-2 mb-1'>Headphones</Link>
+                                <Link className='text-white py-2 mb-1'>Smart Watches</Link>
+                                <Link className='text-white py-2 mb-1'>Tablets</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <footer className='py-4'>
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <p className="text-center mb-0 text-white">
+                                &copy; {new Date().getFullYear()}; Powered by Amazify.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,46 @@ import { BsSearch, bsSearch } from 'react-icons/bs'
 const Header = () => {
     return (
         <>
-            <header className='header-top-strip py-3'>
+            <header className='header-top-strip py-2'>
                 <div className='container-xxl'>
-                    <div className='row'>
-                        <div className='col-6'>
+                    <div className='row align-items-center'>
+                        <div className='col-5'>
                             <p className='text-white mb-0'>Free Shipping Over $100 & Free Returns</p>
                         </div>
-                        <div className='col-6'>
-                            <p className='text-end text-white mb-0'>Hotline: <a href='tel: +919876543210' className='text-white'>+91 9876543210</a></p>
+                        <div className='col-7 d-flex justify-content-end align-items-center gap-30'>
+                            <p className='text-white mb-0'>
+                                Hotline:
+                                <a href='tel: +919876543210' className='text-white'>
+                                    (888) 4344 6000 - (888) 1338 8193
+                                </a>
+                            </p>
+
+                            <div className='dropdown'>
+                                <button className='btn btn-secondary dropdown-toggle bg-transparent border-0 text-white' type='button' id='languageDropdown' data-bs-toggle='dropdown' aria-expanded='false'>
+                                    Language
+                                </button>
+                                <ul className='dropdown-menu' aria-labelledby='languageDropdown'>
+                                    <li><Link className='dropdown-item text-white' to='#'>English</Link></li>
+                                    <li><Link className='dropdown-item text-white' to='#'>Hindi</Link></li>
+                                    <li><Link className='dropdown-item text-white' to='#'>Spanish</Link></li>
+                                </ul>
+                            </div>
+
+                            <div className='dropdown'>
+                                <button className='btn btn-secondary dropdown-toggle bg-transparent border-0 text-white' type='button' id='currencyDropdown' data-bs-toggle='dropdown' aria-expanded='false'>
+                                    Currency
+                                </button>
+                                <ul className='dropdown-menu' aria-labelledby='currencyDropdown'>
+                                    <li><Link className='dropdown-item text-white' to='#'>USD</Link></li>
+                                    <li><Link className='dropdown-item text-white' to='#'>INR</Link></li>
+                                    <li><Link className='dropdown-item text-white' to='#'>EUR</Link></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </header>
-            <header className='header-upper py-3'>
+            <header className='header-upper py-2'>
                 <div className='container-xxl'>
                     <div className='row align-items-center'>
                         <div className='col-2'>
@@ -34,7 +61,7 @@ const Header = () => {
                             </div>
                         </div>
                         <div className='col-5'>
-                            <div className='header-upper-links d-flex align-items-center justify-content-between'>
+                            <div className='header-upper-links d-flex align-items-center justify-content-between gap-20'>
                                 <div>
                                     <Link className='d-flex align-items-center gap-10 text-white' >
                                         <img src='images/compare.svg' alt='compare' />
@@ -67,7 +94,7 @@ const Header = () => {
                     </div>
                 </div>
             </header>
-            <header className='header-bottom py-3'>
+            <header className='header-bottom py-2'>
                 <div className='container-xxl'>
                     <div className='row'>
                         <div className='col-12'>
@@ -85,8 +112,9 @@ const Header = () => {
                                         </ul>
                                     </div>
                                 </div>
+                                <div className="vr-line"></div>
                                 <div className='menu-links'>
-                                    <div className="d-flex align-items-center gap-15">
+                                    <div className="d-flex align-items-center gap-40">
                                         <NavLink to="/">Home</NavLink>
                                         <NavLink to="/">Our Store</NavLink>
                                         <NavLink to="/">Blogs</NavLink>
