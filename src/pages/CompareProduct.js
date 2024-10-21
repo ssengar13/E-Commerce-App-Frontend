f0c66b44744f1d338675b8eb488dd7d5514dd809
Sonal Sengar <sengarsonal13@gmail.com>
2024-10-21 19:55:21 +0530

    worked on compare product page

--- src/pages/CompareProduct.js
+++ src/pages/CompareProduct.js
@@ -18,9 +18,36 @@ const CompareProduct = () => {
                                 <div className="product-card-image">
                                     <img src="images/watch.jpg" alt="watch" className='img-fluid' />
                                 </div>
-                                <div className="compare-product-details">
-                                    <h5 className="title">Milanese Loop Watch Band For 42mm/44mm Apple Watch</h5>
-                                    <h6 className="price">$99.00</h6>
+                            </div>
+                            <div className="compare-product-details">
+                                <h5 className="title">Milanese Loop Watch Band For 42mm/44mm Apple Watch</h5>
+                                <h6 className="price mb-3">$99.00</h6>
+                                <div>
+                                    <div className='product-detail'>
+                                        <h5>Brand:</h5>
+                                        <p className='mb-1'>Kenneth Cole</p>
+                                    </div>
+                                    <div className='product-detail'>
+                                        <h5>Type:</h5>
+                                        <p className='mb-1'>Kenneth Cole</p>
+                                    </div>
+                                    <div className='product-detail'>
+                                        <h5>Availablity:</h5>
+                                        <p className='mb-1'>Kenneth Cole</p>
+                                    </div>
+                                    <div className='product-detail'>
+                                        <h5>Color:</h5>
+                                        <p className='mb-1'>Kenneth Cole</p>
+                                    </div>
+                                    <div className='product-detail'>
+                                        <h5>Size:</h5>
+                                        <div className='d-flex gap-10'>
+                                            <p className='mb-1'>S</p>
+                                            <p className='mb-1'>M</p>
+                                            <p className='mb-1'>L</p>
+                                            <p className='mb-1'>XL</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
